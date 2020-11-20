b99d74fe993bc05e6236979622cff667b050dc87
Marlo <mpa.vandenoetelaar@student.avans.nl>
2020-11-20 11:41:37 +0100

    crud

---
+++ Bumbo.Data/Repository/PrognosesRepository.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Bumbo.Data;
+using Bumbo.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace Bumbo.Data.Repository
+{
+    public class PrognosesRepository : IPrognosesRepository
+    {
+        public List<Prognoses> GetAll()
+        {
+            using (var ctx = new ContextFactory().CreateDbContext(null))
+            {
+                return ctx.Prognoses.ToList();
+            }
+        }
+
+        public Prognoses Get(DateTime date)
+        {
+            using (var ctx = new ContextFactory().CreateDbContext(null))
+            {
+                return ctx.Prognoses.FirstOrDefault(n => n.Date == date.Date);
+            }
+        }
+
+        public Prognoses Create(Prognoses prog)
+        {
+            using (var ctx = new ContextFactory().CreateDbContext(null))
+            {
+                ctx.Prognoses.Add(prog);
+                ctx.SaveChanges();
+                return prog;
+            }
+        }
+
+        public Prognoses Update(Prognoses prog)
+        {
+            using (var ctx = new ContextFactory().CreateDbContext(null))
+            {
+                ctx.Prognoses.Update(prog);
+                ctx.SaveChanges();
+                return prog;
+            }
+        }
+
+        public bool Delete(DateTime date)
+        {
+            using (var ctx = new ContextFactory().CreateDbContext(null))
+            {
+                var toRemove = ctx.Prognoses.Find(date);
+                if (toRemove != null)
+                {
+                    ctx.Prognoses.Remove(toRemove);
+                    ctx.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ Bumbo.Web/Controllers/PrognosisController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Bumbo.Data.Models;
+using Bumbo.Data.Repository;
+using Bumbo.Web.Models;
+using System;
+
+namespace Bumbo.Web.Controllers
+{
+    public class PrognosisController : Controller
+    {
+        private IPrognosesRepository _repo = new PrognosesRepository();
+
+        public IActionResult Index()
+        {
+            if (TempData["Error"] != null) ViewBag.Error = TempData["Error"].ToString();
+
+            ViewBag.Prognoses = _repo.GetAll();
+            return View();
+        }
+
+        public IActionResult Show(DateTime date)
+        {
+            if (TempData["Error"] != null) ViewBag.Error = TempData["Error"].ToString();
+
+            ViewBag.Prognoses = _repo.Get(date.Date);
+            return View("Detail");
+        }
+
+        public IActionResult Create()
+        {
+            if (TempData["Error"] != null) ViewBag.Error = TempData["Error"].ToString();
+
+            var model = new Prognoses();
+            return View("Create", model);
+        }
+
+        [HttpPost]
+        public IActionResult Store(Prognoses prog)
+        {
+            if (prog.AmountOfCustomers == 0)
+            {
+                TempData["Error"] = "Veld mag niet leeg zijn";
+                return RedirectToAction("Create");
+            }
+
+            var model = _repo.Create(prog);
+            return RedirectToAction("Show", new { model.Date });
+        }
+
+        public IActionResult Edit(DateTime date)
+        {
+            if (TempData["Error"] != null) ViewBag.Error = TempData["Error"].ToString();
+
+            var model = _repo.Get(date.Date);
+
+            return View("Edit", model);
+        }
+
+        [HttpPost]
+        public IActionResult Update(DateTime date, Prognoses updatedProg)
+        {
+            if (date.Date != updatedProg.Date) return NotFound();
+
+            if (updatedProg.AmountOfCustomers == 0)
+            {
+                TempData["Error"] = "Veld mag niet leeg zijn";
+                return RedirectToAction("Edit", new { date });
+            }
+
+
+            _repo.Update(updatedProg);
+            return RedirectToAction("Edit", new { date });
+        }
+
+        [HttpPost]
+        public IActionResult Delete(DateTime date)
+        {
+            if (_repo.Get(date.Date) != null)
+            {
+                _repo.Delete(date.Date);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
