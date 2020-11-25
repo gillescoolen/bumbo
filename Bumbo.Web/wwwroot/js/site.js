26d5044d02a26ffcef222f881a115d5447869b7a
Gilles <gilleswhc@gmail.com>
2020-11-25 21:10:02 +0100

    Implement sidebar

--- Bumbo.Web/wwwroot/js/site.js
+++ Bumbo.Web/wwwroot/js/site.js
@@ -2,41 +2,46 @@
 const url = 'api/schedule';
 
 document.addEventListener('DOMContentLoaded', function () {
-  const calendarElement = document.getElementById('calendar');
-  const eventForm = document.getElementById('add-event');
+    const calendarElement = document.getElementById('calendar');
+    const eventForm = document.getElementById('add-event');
 
-  eventForm.addEventListener("submit", (e) => create(e), false);
+    /* todo: replace jquery with vanilla js */
+    $("#menu-toggle").click(function (e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
 
-  const create = () => console.log('create...');
+    const update = () => console.log('update...');
 
+    const create = () => console.log('create...');
 
-  const update = () => console.log('update...');
+    const calendar = new FullCalendar.Calendar(calendarElement, {
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'timeGridWeek'
+        },
 
-  const calendar = new FullCalendar.Calendar(calendarElement, {
-    headerToolbar: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'timeGridWeek'
-    },
+        intitialView: 'timeGridWeek',
+        initialDate: new Date().toISOString().split('T')[0],
 
-    intitialView: 'timeGridWeek',
-    initialDate: new Date().toISOString().split('T')[0],
+        nowIndicator: true,
+        navLinks: true,
+        editable: true,
 
-    nowIndicator: true,
-    navLinks: true,
-    editable: true,
+        eventClick: () => console.log('click...'),
 
-    eventClick: () => console.log('click...'),
+        dateClick: () => console.log('click...'),
 
-    dateClick: () => console.log('click...'),
+        eventDrop: update,
+        eventResize: update,
 
-    eventDrop: update,
-    eventResize: update,
+        events: {
+            url: 'api/schedule',
+        }
+    });
 
-    events: {
-      url: 'api/schedule',
-    }
-  });
+    eventForm.addEventListener("submit", (e) => create(e), false);
 
-  calendar.render();
+    calendar.render();
 });
