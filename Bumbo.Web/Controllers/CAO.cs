1fe05e1248ae2b2645ed5e26fbc9ef7cdf05b74c
Joren <63969493+joren961@users.noreply.github.com>
2020-12-15 16:42:14 +0100

    Update CAO.cs
    Co-Authored-By: Vincent <55784500+CheezyOP@users.noreply.github.com>

--- Bumbo.Web/Controllers/CAO.cs
+++ Bumbo.Web/Controllers/CAO.cs
@@ -43,24 +43,54 @@ namespace Bumbo.Web.Controllers
                 throw new System.ArgumentException("Start time cannot be more than Finish Time, and vice versa");
             }
 
-            if (start.DayOfWeek.Equals("Sunday"))
-            {
-                for (double i = startHour; i < finishHour; i = i + 0.5)
-                {
-                    halfHourWithSubcharge.Add(i, 100);
-                }
-                return halfHourWithSubcharge;
-            } 
-
             //determines subcharge per half hour worked
             for (double i = startHour; i <= finishHour; i=i+0.5)
             {
-                if (start.DayOfWeek.Equals("Saturday"))
+                //nieuwjaar
+                if (start.Month == 1 && start.Day == 1)
+                {
+                    halfHourWithSubcharge.Add(i, 100);
+                }
+                //pasen
+                else if (start.Month == 4 && (start.Day==12 || start.Day==13))
+                {
+                    halfHourWithSubcharge.Add(i, 100);
+                }
+                //koningsdag
+                else if (start.Month == 4 && start.Day==27)
+                {
+                    halfHourWithSubcharge.Add(i, 100);
+                }
+                //bevrijdingsdag
+                else if (start.Month == 5 && start.Day==5)
+                {
+                    halfHourWithSubcharge.Add(i, 100);
+                }
+                //hemelvaartsdag
+                else if (start.Month == 5 && start.Day == 21)
+                {
+                    halfHourWithSubcharge.Add(i, 100);
+                }
+                //pinksteren
+                else if (start.Month == 5 && start.Day == 31 || start.Month==6 && start.Day == 1)
+                {
+                    halfHourWithSubcharge.Add(i, 100);
+                }
+                //kerst
+                else if (start.Month == 12 && start.Day == 25 || start.Month==4 && start.Day==26)
+                {
+                    halfHourWithSubcharge.Add(i, 100);
+                }
+                else if (start.DayOfWeek.Equals("Saturday"))
                 {
                     if (i >= 18.00 && i < 21.00)
                     {
                         halfHourWithSubcharge.Add(i, 50);
                     }
+                }
+                else if (start.DayOfWeek.Equals("Sunday"))
+                {
+                    halfHourWithSubcharge.Add(i, 100);
                 }
                 else if (i >= 21.00 && i <= 24.00)
                 {
@@ -73,21 +103,21 @@ namespace Bumbo.Web.Controllers
                 else if (i >= 0 && i <= 6.00)
                 {
                     halfHourWithSubcharge.Add(i, 50);
+                } 
+                else
+                {
+                    halfHourWithSubcharge.Add(i, 0);
                 }
             }
-
-            //TODO Feestdagen (per feestdatum kijken of date erop valt)
-            
             return halfHourWithSubcharge;
         }
 
         /**
          * Checks if the user suffices the CAO norms for the given work week
          */
-        //TODO Check for > 40 hours worked for 16-17 your olds this month
-        public Boolean ApproveWorkWeek(User user, PlannedWorktime[] plannedWorkWeek)
+        public bool ApproveWorkWeek(User user, PlannedWorktime[] plannedWorkWeek)
         {
-            Boolean isApprovable = StandardNorms(plannedWorkWeek);
+            bool isApprovable = StandardNorms(plannedWorkWeek);
 
             if (!isApprovable)
             {
@@ -127,7 +157,7 @@ namespace Bumbo.Web.Controllers
                 }
             }
 
-            //Checks if worked hours this week is less then 60 hours
+            //Checks if worked hours this week is less than 60 hours
             return totalMinutesWorked >= (60 * 60);
         }
 
@@ -138,7 +168,7 @@ namespace Bumbo.Web.Controllers
         {
             foreach (PlannedWorktime workDay in plannedWorkWeek)
             {
-                //Checks if worked hours + shool hours per day is less then 9 hours
+                //Checks if worked hours + school hours per day is less than 9 hours
                 int? schoolhoursworked = _context.AvailableWorktime.Where(at => at.UserId == user.Id && at.WorkDate == workDay.WorkDate).Select(at => at.SchoolHoursWorked).FirstOrDefault();
                 if ((workDay.Finish.Subtract(workDay.Start).Hours + (int)schoolhoursworked) > 9)
                 {
@@ -146,11 +176,40 @@ namespace Bumbo.Web.Controllers
                 }
             }
 
+            if (!LessThanFortyHoursAverageInMonth(user,plannedWorkWeek[0].WorkDate.Month))
+            {
+                return false;
+            }
+
             return true;
         }
 
+        public bool LessThanFortyHoursAverageInMonth(User user, int month)
+        {
+            double totalHoursInMonth = 0;
+            List<PlannedWorktime> worktimes = _context.PlannedWorktime.Where(u => u.UserId == user.Id && u.WorkDate.Month == month).ToList();
+            foreach (var time in worktimes)
+            {
+                int? schoolhoursworked = _context.AvailableWorktime.Where(at => at.UserId == user.Id && at.WorkDate == time.WorkDate).Select(at => at.SchoolHoursWorked).FirstOrDefault();
+                if (schoolhoursworked==null)
+                {
+                    schoolhoursworked = 0;
+                }
+                totalHoursInMonth = totalHoursInMonth + (int)schoolhoursworked + time.Finish.Subtract(time.Start).Hours;
+            }
+            //1 month has roughly 4.34812141 weeks, rond omlaag af omdat het beter is te veel uren te tellen dan te weinig ivm risico
+            if ((totalHoursInMonth / 4.34) < 40)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
         /**
-         * Returns value based on if the standard CAO norms are met for employees younger then 16 years
+         * Returns value based on if the standard CAO norms are met for employees younger than 16 years
          */
         private bool UnderSixteenNorms(User user, PlannedWorktime[] plannedWorkWeek)
         {
