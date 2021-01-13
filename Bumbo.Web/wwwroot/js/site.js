1c0c3930671414c16c7e0a26026d172037fb6cab
Gilles <gilles@coolen.dev>
2021-01-13 21:58:15 +0100

    Fix __k missing console error

--- Bumbo.Web/wwwroot/js/site.js
+++ Bumbo.Web/wwwroot/js/site.js
@@ -1,41 +1,4 @@
 ﻿// @ts-nocheck
-const url = 'api/schedule';
-
-document.addEventListener('DOMContentLoaded', function () {
-    const calendarElement = document.getElementById('calendar');
-    const eventForm = document.getElementById('add-event');
-
-    /* todo: replace jquery with vanilla js */
-    $("#menu-toggle").click(function (e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
-
-
-    const calendar = new FullCalendar.Calendar(calendarElement, {
-        headerToolbar: {
-            left: 'prev,next today',
-            center: 'title',
-            right: ''
-        },
-
-        initialView: 'timeGridWeek',
-        initialDate: new Date().toISOString().split('T')[0],
-
-        nowIndicator: true,
-        navLinks: true,
-        editable: false,
-
-        events: {
-            url,
-        }
-    });
-
-    calendar.render();
-});
-
-
-//TODO: Vanilla js?
 $(function () {
     $("input.customerAmount").focusout(() => changeWorkingHours(this));
     $("input.freightAmount").focusout(() => changeWorkingHours(this));
